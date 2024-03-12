0e9e58739d8bdbd29f1ed9d7e9af4b96b700a464
Ada Taylor <ada@idu.com>
2024-03-12 16:10:36 -0400

    Tweak to handling of pose objs

--- src/tb_lattice.cpp
+++ src/tb_lattice.cpp
@@ -232,7 +232,7 @@ bool AStarLattice::findReplanningWaypoint(ros::Time conti_time,
       ROS_DEBUG("turn in place. update pose with amcl");
       // tf::Stamped<tf::Pose> robot_pose;
       geometry_msgs::PoseStamped robot_pose;
-      if(dynamic_costmap_->getStaticROSCostmap()->getRobotPose(robot_pose_stamped))
+      if(dynamic_costmap_->getStaticROSCostmap()->getRobotPose(robot_pose))
       {
         conti_pose.pose.position.x = robot_pose.pose.getOrigin().getX();
         conti_pose.pose.position.y = robot_pose.pose.getOrigin().getY();
