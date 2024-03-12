0123b75cfd48a6a4aad7037d8e0fdc4f15e44532
Ada Taylor <ada@idu.com>
2024-03-12 13:14:15 -0400

    Additional imports

--- include/lattice_planner/static_costmap.h
+++ include/lattice_planner/static_costmap.h
@@ -53,6 +53,8 @@
 #include <lattice_planner/SocialCostmapConfig.h>
 #include <tf2/utils.h>
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
 
 #include <pluginlib/class_list_macros.h>
 
